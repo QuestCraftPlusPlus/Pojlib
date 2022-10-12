2da77640a52234fdac8cbbe926cb5cfc46cb7edf
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-10-12 19:28:08 -0400

    Pojlib Rename

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/7.5.1/userguide/multi_project_builds.html
  */
 
-rootProject.name = "Polib-Unity"
+rootProject.name = "Pojlib-Unity"
 include("lib")

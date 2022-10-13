fedba072b366f5bea11d130f982493de8ff73ba4
MrNavaStar <Mr.NavaStar@gmail.com>
2022-10-12 19:22:34 -0600

    Downloading libs + assets works

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,11 +20,10 @@ dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+    implementation("com.google.code.gson:gson:2.9.1")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("commons-io:commons-io:2.11.0")
+    implementation("commons-codec:commons-codec:1.15")
 }
 
 tasks.named<Test>("test") {
---
+++ lib/src/main/java/pojlib/unity/util/APIHandler.java
@@ -0,0 +1,58 @@
+package pojlib.unity.util;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class APIHandler {
+
+    private final String baseUrl;
+
+    public APIHandler(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    private String parseQueries(HashMap<String, Object> query) {
+        StringBuilder params = new StringBuilder("?");
+        for (String param : query.keySet()) {
+            Object value = query.get(param);
+            params.append(param).append("=").append(value).append("&");
+        }
+        return params.substring(0, params.length() - 1);
+    }
+
+    public <T> T get(String endpoint, Class<T> tClass) {
+        return new Gson().fromJson(getRaw(baseUrl + "/" + endpoint), tClass);
+    }
+
+    public <T> T get(String endpoint, HashMap<String, Object> query, Class<T> tClass) {
+        return get(endpoint + parseQueries(query), tClass);
+    }
+
+    //Make a get request and return the response as a raw string;
+    public static String getRaw(String url) {
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            InputStream inputStream = conn.getInputStream();
+            String data = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+            inputStream.close();
+            conn.disconnect();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

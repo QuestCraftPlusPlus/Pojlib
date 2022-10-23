8d32365b92f613ef0e2d9764f7dba41ee054d24b
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-10-23 04:05:26 -0400

    Update Constants

--- lib/src/main/java/pojlib/unity/util/Constants.java
+++ lib/src/main/java/pojlib/unity/util/Constants.java
@@ -4,4 +4,11 @@ public class Constants {
 
     public static String MOJANG_API_URL = "https://piston-meta.mojang.com";
     public static String MOJANG_RESOURCES_URL = "https://resources.download.minecraft.net";
+
+    public static String OAUTH_TOKEN_URL = "https://login.live.com/oauth20_token.srf";
+    public static String XBL_AUTH_URL = "https://user.auth.xboxlive.com/user/authenticate";
+    public static String XSTS_AUTH_URL = "https://xsts.auth.xboxlive.com/xsts/authorize";
+    public static String MC_LOGIN_URL = "https://api.minecraftservices.com/authentication/login_with_xbox";
+    public static String MC_STORE_URL = "https://api.minecraftservices.com/entitlements/mcstore";
+    public static String MC_PROFILE_URL = "https://api.minecraftservices.com/minecraft/profile";
 }

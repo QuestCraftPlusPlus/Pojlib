d4e8b4ee6f12482c781be2548cb70dc060a45d04
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-10-23 04:41:55 -0400

    Separate MSA | Remove Unused Classes

--- lib/src/main/java/pojlib/unity/util/RefreshListener.java
+++
@@ -1,9 +0,0 @@
-package pojlib.unity.util;
-
-import pojlib.unity.authentication.MinecraftAccount;
-
-public interface RefreshListener
-{
-    public void onFailed(Throwable e);
-    public void onSuccess(MinecraftAccount profile);
-}

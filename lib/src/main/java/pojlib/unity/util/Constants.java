2b4b75d2adf07b9a63487180530a4bb854d43b60
MrNavaStar <Mr.NavaStar@gmail.com>
2022-10-23 02:09:07 -0600

    Refactor installer system + add fabric/quilt install + add API

--- lib/src/main/java/pojlib/unity/util/Constants.java
+++ lib/src/main/java/pojlib/unity/util/Constants.java
@@ -2,6 +2,9 @@ package pojlib.unity.util;
 
 public class Constants {
 
-    public static String MOJANG_API_URL = "https://piston-meta.mojang.com";
+    public static String MOJANG_META_URL = "https://piston-meta.mojang.com";
     public static String MOJANG_RESOURCES_URL = "https://resources.download.minecraft.net";
+
+    public static String FABRIC_META_URL = "https://meta.fabricmc.net/v2";
+    public static String QUILT_META_URL = "https://meta.quiltmc.org/v3";
 }

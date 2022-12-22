c331d8668776e6796dd75dabe642accadd8eb0d5
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-12-21 17:23:37 -0800

    Automatic mod download

--- lib/src/main/java/pojlib/util/JREUtils.java
+++ lib/src/main/java/pojlib/util/JREUtils.java
@@ -190,7 +190,7 @@ public class JREUtils {
         // return ldLibraryPath;
     }
 
-    public static int launchJavaVM(final Activity activity,final List<String> JVMArgs) throws Throwable {
+    public static int launchJavaVM(final Activity activity,final List<String> JVMArgs, String versionName) throws Throwable {
         JREUtils.relocateLibPath(activity);
         setJavaEnvironment(activity);
 
@@ -203,6 +203,7 @@ public class JREUtils {
         userArgs.add("-Dorg.lwjgl.opengl.libname=" + graphicsLib);
         userArgs.add("-Dorg.lwjgl.opengles.libname=" + "/system/lib64/libGLESv3.so");
         userArgs.add("-Dorg.lwjgl.egl.libname=" + "/system/lib64/libEGL.so");
+        userArgs.add("-Dfabric.addMods=" + Constants.MC_DIR + "/mods/" + versionName);
 
         userArgs.addAll(JVMArgs);
         System.out.println(JVMArgs);

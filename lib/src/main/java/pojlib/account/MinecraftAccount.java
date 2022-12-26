15a47c51e995fb4a7ce248489c429782b6c9a09c
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-12-26 14:10:43 -0800

    Refresh token every login

--- lib/src/main/java/pojlib/account/MinecraftAccount.java
+++ lib/src/main/java/pojlib/account/MinecraftAccount.java
@@ -36,7 +36,7 @@ public class MinecraftAccount {
     public static MinecraftAccount login(String gameDir, JsonObject response) throws IOException, JSONException {
         MinecraftAccount account = Msa.acquireXBLToken(response.get("access_token").getAsString());
         account.msaRefreshToken = response.get("refresh_token").getAsString();
-        account.expiresIn = Instant.now().plusSeconds(response.get("expires_in").getAsInt()).getNano();
+        account.expiresIn = response.get("expires_in").getAsInt();
 
         GsonUtils.objectToJsonFile(gameDir + "/account.json", account);
         return account;
@@ -51,30 +51,28 @@ public class MinecraftAccount {
     public static MinecraftAccount load(String path, String client_id) {
         try {
             MinecraftAccount acc = new Gson().fromJson(new FileReader(path + "/account.json"), MinecraftAccount.class);
-            if(Instant.now().getNano() >= acc.expiresIn) {
-                try {
-                    URLConnection connection2 = new URL("https://login.microsoftonline.com/consumers/oauth2/v2.0/token").openConnection();
-                    connection2.setDoOutput(true);
-                    connection2.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            try {
+                URLConnection connection2 = new URL("https://login.microsoftonline.com/consumers/oauth2/v2.0/token").openConnection();
+                connection2.setDoOutput(true);
+                connection2.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-                    String query = String.format("client_id=%s&grant_type=%s&refresh_token=%s",
-                            URLEncoder.encode(client_id, "UTF-8"),
-                            URLEncoder.encode("refresh_token", "UTF-8"),
-                            URLEncoder.encode(acc.msaRefreshToken, "UTF-8"));
-                    try (OutputStream output = connection2.getOutputStream()) {
-                        output.write(query.getBytes(StandardCharsets.UTF_8));
-                    }
-
-                    InputStream response = connection2.getInputStream();
-                    JsonObject jsonObject = (JsonObject) JsonParser.parseReader(new InputStreamReader(response, StandardCharsets.UTF_8));
-
-                    acc = Msa.acquireXBLToken(jsonObject.get("access_token").getAsString());
-                    acc.msaRefreshToken = jsonObject.get("refresh_token").getAsString();
-                    acc.expiresIn = Instant.now().plusSeconds(jsonObject.get("expires_in").getAsInt()).getNano();
-                    GsonUtils.objectToJsonFile(path + "/account.json", acc);
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
+                String query = String.format("client_id=%s&grant_type=%s&refresh_token=%s",
+                        URLEncoder.encode(client_id, "UTF-8"),
+                        URLEncoder.encode("refresh_token", "UTF-8"),
+                        URLEncoder.encode(acc.msaRefreshToken, "UTF-8"));
+                try (OutputStream output = connection2.getOutputStream()) {
+                    output.write(query.getBytes(StandardCharsets.UTF_8));
                 }
+
+                InputStream response = connection2.getInputStream();
+                JsonObject jsonObject = (JsonObject) JsonParser.parseReader(new InputStreamReader(response, StandardCharsets.UTF_8));
+
+                acc = Msa.acquireXBLToken(jsonObject.get("access_token").getAsString());
+                acc.msaRefreshToken = jsonObject.get("refresh_token").getAsString();
+                acc.expiresIn = jsonObject.get("expires_in").getAsInt();
+                GsonUtils.objectToJsonFile(path + "/account.json", acc);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
             }
             return acc;
         } catch (FileNotFoundException e) {

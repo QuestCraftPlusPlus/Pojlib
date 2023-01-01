83422a82b4c1d52a70e7f0d3ea8ebf7c76ab192e
CADIndie <84008186+CADIndie@users.noreply.github.com>
2023-01-01 03:02:38 -0500

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,18 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.withType<Test>{
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
 android {
     compileSdkVersion = "android-31"
     defaultConfig {

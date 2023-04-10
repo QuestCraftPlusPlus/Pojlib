91a3677d8fdeab4ae1afc4b3f2248251864920fb
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2023-04-10 17:42:49 -0400

    4.0 Refactor | Update PR Push (#7)
    I literally have no clue what Git did...

--- lib/src/main/java/pojlib/util/Constants.java
+++ lib/src/main/java/pojlib/util/Constants.java
@@ -7,24 +7,28 @@ import java.io.File;
 public class Constants {
 
     public static String MOJANG_META_URL = "https://piston-meta.mojang.com";
-    public static String MOJANG_RESOURCES_URL = "https://resources.download.minecraft.net";
-    
-    public static String FABRIC_META_URL = "https://meta.fabricmc.net/v2";
 
-    //TODO: Implement Correct API Endpoints.
-    public static String FORGE_META_URL = "https://mrnavastar.github.io/ForgeVersionAPI/";
+    public static String MOJANG_RESOURCES_URL = "https://resources.download.minecraft.net";
+
+    public static String FABRIC_META_URL = "https://meta.fabricmc.net/v2";
 
     public static String QUILT_META_URL = "https://meta.quiltmc.org/v3";
 
     public static String OAUTH_TOKEN_URL = "https://login.live.com/oauth20_token.srf";
+
     public static String XBL_AUTH_URL = "https://user.auth.xboxlive.com/user/authenticate";
+
     public static String XSTS_AUTH_URL = "https://xsts.auth.xboxlive.com/xsts/authorize";
+
     public static String MC_LOGIN_URL = "https://api.minecraftservices.com/authentication/login_with_xbox";
+
     public static String MC_STORE_URL = "https://api.minecraftservices.com/entitlements/mcstore";
+
     public static String MC_PROFILE_URL = "https://api.minecraftservices.com/minecraft/profile";
 
     public static String CRAFATAR_URL = "https://crafatar.com";
 
     public static String MC_DIR = new File(Environment.getExternalStorageDirectory(),"Android/data/com.qcxr.qcxr/files/.minecraft").getAbsolutePath();
+
     public static String USER_HOME = new File(Environment.getExternalStorageDirectory(),"Android/data/com.qcxr.qcxr/files").getAbsolutePath();
 }

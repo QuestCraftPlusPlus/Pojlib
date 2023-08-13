6444280e9a1496f0ab0645f399151c4ec52ca8a3
The Judge <colepatrickroberts@gmail.com>
2023-08-13 14:33:50 -0700

    Fixes

--- lib/src/main/java/pojlib/util/JREUtils.java
+++ lib/src/main/java/pojlib/util/JREUtils.java
@@ -194,8 +194,8 @@ public class JREUtils {
             userArgs.add("-Xms" + API_V1.memoryValue + "M");
             userArgs.add("-Xmx" + API_V1.memoryValue + "M");
         } else {
-            userArgs.add("-Xms" + 2048 + "M");
-            userArgs.add("-Xmx" + 2048 + "M");
+            userArgs.add("-Xms" + 1024 + "M");
+            userArgs.add("-Xmx" + 1024 + "M");
         }
 
         userArgs.add("-Dorg.lwjgl.opengl.libname=" + graphicsLib);

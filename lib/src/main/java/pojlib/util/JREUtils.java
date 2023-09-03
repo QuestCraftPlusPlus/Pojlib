894bd7ec61c33deb34f2a1c4c096aef34bfed659
The Judge <colepatrickroberts@gmail.com>
2023-09-03 15:40:50 -0700

    If you have already logged in online, allow login offline

--- lib/src/main/java/pojlib/util/JREUtils.java
+++ lib/src/main/java/pojlib/util/JREUtils.java
@@ -194,8 +194,8 @@ public class JREUtils {
             userArgs.add("-Xms" + API_V1.memoryValue + "M");
             userArgs.add("-Xmx" + API_V1.memoryValue + "M");
         } else {
-            userArgs.add("-Xms" + 1024 + "M");
-            userArgs.add("-Xmx" + 1024 + "M");
+            userArgs.add("-Xms" + 1800 + "M");
+            userArgs.add("-Xmx" + 1800 + "M");
         }
 
         userArgs.add("-Dorg.lwjgl.opengl.libname=" + graphicsLib);

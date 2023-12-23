d30c9b846c9b174eb51419294f4483df5ab3c3f4
TheJudge <colepatrickroberts@gmail.com>
2023-12-23 13:23:36 -0800

    Refactor CoreMod Handling
    *Why did I do this like this in the first place*

---
+++ lib/src/main/java/pojlib/util/CoreMods.java
@@ -0,0 +1,21 @@
+package pojlib.util;
+
+import java.util.List;
+
+public class CoreMods {
+    public List<Version> versions;
+
+    public static class Version {
+        public String name;
+        public List<Mod> mods;
+        public Version(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class Mod {
+        public String slug;
+        public String version;
+        public String download_link;
+    }
+}

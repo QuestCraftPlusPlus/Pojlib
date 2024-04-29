2cf1f019ee9037cefaf5d798bac8b645e542ba72
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2024-04-29 00:10:04 -0400

    Add Archive Extractor

--- src/main/java/pojlib/util/FileUtil.java
+++ src/main/java/pojlib/util/FileUtil.java
@@ -1,11 +1,20 @@
 package pojlib.util;
 
+import static pojlib.UnityPlayerActivity.newFile;
+
+import android.app.Activity;
 import android.content.Context;
+
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import pojlib.api.API_V1;
 
 public class FileUtil {
 
@@ -54,4 +63,41 @@ public class FileUtil {
         fos.close();
     }
 
+    public static void UnzipArchive(String archivePath, String archiveName, Activity activity) {
+        try {
+            File archive = new File(archivePath);
+            FileUtils.writeByteArrayToFile(archive, FileUtil.loadFromAssetToByte(activity, archiveName));
+            byte[] buffer = new byte[1024];
+            ZipInputStream zis = new ZipInputStream(Files.newInputStream(archive.toPath()));
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = newFile(new File(Constants.USER_HOME + "/" + archiveName), zipEntry);
+                if (zipEntry.isDirectory()) {
+                    if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                        throw new IOException("Failed to create directory " + newFile);
+                    }
+                } else {
+                    // fix for Windows-created archives
+                    File parent = newFile.getParentFile();
+                    if (!parent.isDirectory() && !parent.mkdirs()) {
+                        throw new IOException("Failed to create directory " + parent);
+                    }
+
+                    // write file content
+                    FileOutputStream fos = new FileOutputStream(newFile);
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                    fos.close();
+                }
+                zipEntry = zis.getNextEntry();
+            }
+
+            zis.closeEntry();
+            zis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

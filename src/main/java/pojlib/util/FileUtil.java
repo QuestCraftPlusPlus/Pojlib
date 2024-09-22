2dc9124848152ae156aa7903d5f20d39d0e7999f
Darien Johnson <84008186+CADIndie@users.noreply.github.com>
2024-09-22 18:10:37 -0500

    Implement Pojav Input System (#59)
    * Implement Pojav Input Backend
    
    * Add Gamepad Compat
    
    * Integrate Gamepad Input
    
    ---------
    
    Co-authored-by: Maksim Belov <45949002+artdeell@users.noreply.github.com>
    Co-authored-by: Mathias Boulay <mathias.boulay@etu.umontpellier.fr>
    Co-authored-by: Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>

--- src/main/java/pojlib/util/FileUtil.java
+++ src/main/java/pojlib/util/FileUtil.java
@@ -141,4 +141,29 @@ public class FileUtil {
 
         return destFile;
     }
+
+    /**
+     * @author PojavLauncherTeam
+     * Same as ensureDirectorySilently(), but throws an IOException telling why the check failed.
+     * @param targetFile the directory to check
+     * @throws IOException when the checks fail
+     */
+    public static void ensureDirectory(File targetFile) throws IOException{
+        if(targetFile.isFile()) throw new IOException("Target directory is a file");
+        if(targetFile.exists()) {
+            if(!targetFile.canWrite()) throw new IOException("Target directory is not writable");
+        }else if(!targetFile.mkdirs()) throw new IOException("Unable to create target directory");
+    }
+
+    /**
+     * @author PojavLauncherTeam
+     * Same as ensureParentDirectorySilently(), but throws an IOException telling why the check failed.
+     * @param targetFile the File whose parent should be checked
+     * @throws IOException when the checks fail
+     */
+    public static void ensureParentDirectory(File targetFile) throws IOException{
+        File parentFile = targetFile.getParentFile();
+        if(parentFile == null) throw new IOException("targetFile does not have a parent");
+        ensureDirectory(parentFile);
+    }
 }

9721d87b354118f1553f15634fac75de34583fb7
The Judge <colepatirckroberts@gmail.com>
2024-09-22 19:00:27 -0700

    Refactor mod handling (again)

--- src/main/java/pojlib/util/json/ProjectInfo.java
+++ src/main/java/pojlib/util/json/ProjectInfo.java
@@ -3,6 +3,6 @@ package pojlib.util.json;
 public class ProjectInfo {
     public String slug;
     public String version;
-    public String type;
+    public String type = "mod";
     public String download_link;
 }

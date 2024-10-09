4fed89d15dd3d5889b80a57610037b7f9b52d3e0
Cart <81428538+cartrigger@users.noreply.github.com>
2024-10-09 02:31:30 -0400

    Actually change the supportedversions to use 5.1.0 instead of testing

--- src/main/java/pojlib/APIHandler.java
+++ src/main/java/pojlib/APIHandler.java
@@ -107,7 +107,7 @@ public class APIHandler {
         return new Gson().fromJson(postRaw(url + parseQueries(query), body.toString()), tClass);
     }
 
-    public static final String SUPPORTED_VERSIONS = "https://raw.githubusercontent.com/QuestCraftPlusPlus/Pojlib/Testing/supportedVersions.json";
+    public static final String SUPPORTED_VERSIONS = "https://raw.githubusercontent.com/QuestCraftPlusPlus/Pojlib/refs/heads/QuestCraft-5.1.0/supportedVersions.json";
 
     public static String[] getQCSupportedVersions() {
         File versionsJson = new File(Constants.USER_HOME + "/supportedVersions.json");

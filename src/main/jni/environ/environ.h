c0c6ab996ed28ab843e3b0fed883270b58a57283
The Judge <colepatirckroberts@gmail.com>
2024-10-21 14:05:18 -0700

    More OpenXR work

--- src/main/jni/environ/environ.h
+++ src/main/jni/environ/environ.h
@@ -54,8 +54,6 @@ struct pojav_environ_s {
     JavaVM* dalvikJavaVMPtr;
     JNIEnv* dalvikJNIEnvPtr_ANDROID;
     jobject activity;
-    XrInstanceCreateInfoAndroidKHR *XRAndroidInstanceBinding;
-    XrGraphicsBindingOpenGLESAndroidKHR *XRAndroidGLESBinding;
     long showingWindow;
     bool isInputReady, isCursorEntered, isUseStackQueueCall, shouldUpdateMouse;
     int savedWidth, savedHeight;
--- src/main/jni/vloader.cpp
+++ src/main/jni/vloader.cpp
@@ -18,66 +18,79 @@
 
 extern "C"
 JNIEXPORT jlong JNICALL
-Java_org_vivecraft_util_VLoader_getEGLGlobal(JNIEnv* env, jclass clazz) {
-    if(pojav_environ->XRAndroidGLESBinding == nullptr) {
-        EGLConfig cfg;
-        EGLint num_configs;
-
-        static const EGLint attribs[] = {
-                EGL_RED_SIZE, 8,
-                EGL_GREEN_SIZE, 8,
-                EGL_BLUE_SIZE, 8,
-                EGL_ALPHA_SIZE, 8,
-                // Minecraft required on initial 24
-                EGL_DEPTH_SIZE, 24,
-                EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
-                EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
-                EGL_NONE
-        };
-
-        eglChooseConfig(eglGetCurrentDisplay(), attribs, &cfg, 1, &num_configs);
-        pojav_environ->XRAndroidGLESBinding = new XrGraphicsBindingOpenGLESAndroidKHR{
-                XR_TYPE_GRAPHICS_BINDING_OPENGL_ES_ANDROID_KHR,
-                nullptr,
-                (void *) eglGetCurrentDisplay(),
-                (void *) cfg,
-                (void *) eglGetCurrentContext()
-        };
-    }
-
-    return reinterpret_cast<jlong>(pojav_environ->XRAndroidGLESBinding);
+Java_org_vivecraft_util_VLoader_getEGLDisplay(JNIEnv* env, jclass clazz) {
+    return reinterpret_cast<jlong>(eglGetCurrentDisplay());
 }
 
 extern "C"
 JNIEXPORT jlong JNICALL
-Java_org_vivecraft_util_VLoader_getAndroidCreateInfo(JNIEnv* env, jclass clazz) {
+Java_org_vivecraft_util_VLoader_getEGLContext(JNIEnv* env, jclass clazz) {
+    return reinterpret_cast<jlong>(eglGetCurrentContext());
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_org_vivecraft_util_VLoader_getEGLConfig(JNIEnv* env, jclass clazz) {
+    EGLConfig cfg;
+    EGLint num_configs;
+
+    static const EGLint attribs[] = {
+            EGL_RED_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_BLUE_SIZE, 8,
+            EGL_ALPHA_SIZE, 8,
+            // Minecraft required on initial 24
+            EGL_DEPTH_SIZE, 24,
+            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
+            EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+            EGL_NONE
+    };
+
+    eglChooseConfig(eglGetCurrentDisplay(), attribs, &cfg, 1, &num_configs);
+    return reinterpret_cast<jlong>(cfg);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_org_vivecraft_util_VLoader_initOpenXRLoader(JNIEnv* env, jclass clazz) {
     PFN_xrInitializeLoaderKHR initializeLoader = nullptr;
     XrResult res;
 
     res = xrGetInstanceProcAddr(XR_NULL_HANDLE, "xrInitializeLoaderKHR",
                                 (PFN_xrVoidFunction *) (&initializeLoader));
 
-    if(!XR_SUCCEEDED(res)) {
+    if (!XR_SUCCEEDED(res)) {
         printf("xrGetInstanceProcAddr returned %d.\n", res);
     }
 
     XrLoaderInitInfoAndroidKHR loaderInitInfoAndroidKhr = {
             XR_TYPE_LOADER_INIT_INFO_ANDROID_KHR,
             nullptr,
-            (void*) pojav_environ->dalvikJavaVMPtr,
-            (void*) pojav_environ->activity
+            (void *) pojav_environ->dalvikJavaVMPtr,
+            (void *) pojav_environ->activity
     };
 
     res = initializeLoader((const XrLoaderInitInfoBaseHeaderKHR *) &loaderInitInfoAndroidKhr);
-    if(!XR_SUCCEEDED(res)) {
+    if (!XR_SUCCEEDED(res)) {
         printf("xrInitializeLoaderKHR returned %d.\n", res);
     }
 
-    JNIEnv* newEnv;
+    JNIEnv *newEnv;
     pojav_environ->dalvikJavaVMPtr->AttachCurrentThread(&newEnv, NULL);
-
-    return reinterpret_cast<jlong>(pojav_environ->XRAndroidInstanceBinding);
 }
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_org_vivecraft_util_VLoader_getDalvikVM(JNIEnv* env, jclass clazz) {
+    return reinterpret_cast<jlong>(pojav_environ->dalvikJavaVMPtr);
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_org_vivecraft_util_VLoader_getDalvikActivity(JNIEnv* env, jclass clazz) {
+    return reinterpret_cast<jlong>(pojav_environ->activity);
+}
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_pojlib_util_VLoader_setAndroidInitInfo(JNIEnv *env, jclass clazz, jobject ctx) {
